46a29b91f3cb3c3596a601ab4248e197173908e9
Vicki Marshall <vickimars@hotmail.com>
2017-04-02 14:02:47 +0100

    initial commit to local repo

--- index.php
+++ index.php
@@ -11,7 +11,7 @@ and open the template in the editor.
     </head>
     <body>
         <?php
-        echo "please work";
+        echo "please work better this time";
         ?>
     </body>
 </html>
