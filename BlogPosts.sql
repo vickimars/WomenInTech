493885410a26964a33255d94ee7f24301c8c9d98
Devina <devina.grewal@gmail.com>
2017-04-04 20:11:55 +0100

    corrected table users

--- BlogPosts.sql
+++ BlogPosts.sql
@@ -7,8 +7,7 @@ username varchar (50) NOT NULL)*/
 
 insert BlogPosts(Title, Post, Author, username)
 values ('Women2017', 'This is all about how many women had gone into IT in 2017', 'Devina', 'Dee0610'),
-			('TechnoStory', 'This is all about how technology can assist with daily tasks', 'Vicki', 'Vicki123'),
-            ('Wo-man2017', 'This is all about how many women beat men in IT in 2017', 'Aimi', 'Aimi2017'),
-             ('Power Of Women', 'This is all about how women are now earning more than men', 'Christina', 'Chrissy234');
+	('TechnoStory', 'This is all about how technology can assist with daily tasks', 'Vicki', 'Vicki123'),
+       ('Wo-man2017', 'This is all about how many women beat men in IT in 2017', 'Aimi', 'Aimi2017')          ('Power Of Women', 'This is all about how women are now earning more than men', 'Christina', 'Chrissy234');
            
 			
