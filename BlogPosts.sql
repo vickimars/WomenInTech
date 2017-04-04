def280db436695bd093786e4ea5d90d51939c582
Devina <devina.grewal@gmail.com>
2017-04-04 16:09:38 +0100

    TABLES
    TABLES

---
+++ BlogPosts.sql
@@ -0,0 +1,14 @@
+/*create table BlogPosts(
+id int primary key auto_increment,
+Title varchar (100) NOT NULL, 
+Post varchar (1000) NOT NULL, 
+Author varchar (50) NOT NULL, 
+username varchar (50) NOT NULL)*/
+
+insert BlogPosts(Title, Post, Author, username)
+values ('Women2017', 'This is all about how many women had gone into IT in 2017', 'Devina', 'Dee0610'),
+			('TechnoStory', 'This is all about how technology can assist with daily tasks', 'Vicki', 'Vicki123'),
+            ('Wo-man2017', 'This is all about how many women beat men in IT in 2017', 'Aimi', 'Aimi2017'),
+             ('Power Of Women', 'This is all about how women are now earning more than men', 'Christina', 'Chrissy234');
+           
+			
