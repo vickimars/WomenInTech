2d7e306a0dcb0c43b25f4d34020c20a8243c945d
Jenny <junnaileung@gmail.com>
2017-04-09 18:32:31 +0100

    Changes

--- Controller/loginval.php
+++ Controller/loginval.php
@@ -1,6 +1,6 @@
 <?php
 
-include functions.php;
+include 'functions.php';
       
 if($_SERVER['REQUEST_METHOD'] == 'POST') {
    
--- Forms/registerform.php
+++ Forms/registerform.php
@@ -1,16 +1,6 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-=======
->>>>>>> refs/remotes/origin/master
 <!--<<<<<<< HEAD
-
 =======
 >>>>>>> refs/remotes/origin/master -->
-<<<<<<< HEAD
->>>>>>> refs/remotes/origin/master
-=======
->>>>>>> refs/remotes/origin/master
 <!DOCTYPE HTML>  
 <html>
 <head>
@@ -23,11 +13,9 @@
 <?php
 include 'connection.php';
 include 'functions.php';
-
 // define variables and set to empty values
 $usernameErr = $emailErr = $passwordErr = "";
 $username = $email = $password = $bio = "";
-
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
   if (empty($_POST["username"])) {
     $usernameErr = "Username is required";
@@ -46,14 +34,12 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
   } else {
     $password = test_input($_POST["password"]);
   }
-
   if (empty($_POST["bio"])) {
     $bio = "";
   } else {
     $bio = test_input($_POST["bio"]);
   }
 }
-
 function test_input($data) {
   $data = trim($data);
   $data = stripslashes($data);
@@ -89,7 +75,6 @@ echo "<br>";
 echo $password;
 echo "<br>";
 echo $bio;
-
 add_user($pdo,$_POST);
 ?>
 
