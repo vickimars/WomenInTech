50c35474722a452bb95b381a2d4c8bb544ed5f96
Aimi E <nur.aimi@gmail.com>
2017-04-14 14:27:31 +0100

    register

--- vickitesting.php
+++ vickitesting.php
@@ -5,6 +5,8 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+
+include 'Forms/connection.php';
 include 'Forms/functions.php';
 include 'Forms/register.php';
 ?>
