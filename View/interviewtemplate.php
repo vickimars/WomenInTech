e71e149aeae92c7d07bac500662a7b329116f608
Vicki Marshall <vickimars@hotmail.com>
2017-04-14 16:29:47 +0100

    new interview form template

---
+++ View/interviewtemplate.php
@@ -0,0 +1,40 @@
+!DOCTYPE html>
+</head>
+			</form>			</div><!-- #search-header -->
+		</div><!-- .site-header-inner -->
+	</header><!-- #masthead -->
+
+				
+	<div id="content" class="site-content">
+
+	<div class="site-content-inner">
+		<div id="primary" class="content-area">
+			<main id="main" class="site-main" role="main">
+
+				
+					
+<article id="post-450" class="post-450 post type-post status-publish format-standard has-post-thumbnail hentry category-data category-feminism category-programming category-talking-to category-tech category-tech-industry">
+	<header class="entry-header">
+					<div class="post-thumbnail">
+				<img width="1175" height="725" src="https://womenoftech.files.wordpress.com/2016/07/talking-to-3.jpg?w=1175&amp;h=725&amp;crop=1" class="attachment-gazette-single-thumbnail size-gazette-single-thumbnail wp-post-image" alt="Talking to Women of Tech" srcset="https://womenoftech.files.wordpress.com/2016/07/talking-to-3.jpg 1175w, https://womenoftech.files.wordpress.com/2016/07/talking-to-3.jpg?w=150&amp;h=93&amp;crop=1 150w, https://womenoftech.files.wordpress.com/2016/07/talking-to-3.jpg?w=300&amp;h=185&amp;crop=1 300w, https://womenoftech.files.wordpress.com/2016/07/talking-to-3.jpg?w=768&amp;h=474&amp;crop=1 768w, https://womenoftech.files.wordpress.com/2016/07/talking-to-3.jpg?w=1024&amp;h=632&amp;crop=1 1024w" sizes="(max-width: 1175px) 100vw, 1175px" data-attachment-id="380" data-permalink="https://womenoftech.wordpress.com/2016/07/05/talking-to-hannah-atkinson/talking-to-3/" data-orig-file="https://womenoftech.files.wordpress.com/2016/07/talking-to-3.jpg" data-orig-size="1175,725" data-comments-opened="1" data-image-meta="{&quot;aperture&quot;:&quot;0&quot;,&quot;credit&quot;:&quot;&quot;,&quot;camera&quot;:&quot;&quot;,&quot;caption&quot;:&quot;&quot;,&quot;created_timestamp&quot;:&quot;0&quot;,&quot;copyright&quot;:&quot;&quot;,&quot;focal_length&quot;:&quot;0&quot;,&quot;iso&quot;:&quot;0&quot;,&quot;shutter_speed&quot;:&quot;0&quot;,&quot;title&quot;:&quot;&quot;,&quot;orientation&quot;:&quot;0&quot;}" data-image-title="Talking to Women of Tech" data-image-description="&lt;p&gt;Talking to Women of Tech&lt;/p&gt;
+" data-medium-file="https://womenoftech.files.wordpress.com/2016/07/talking-to-3.jpg?w=300" data-large-file="https://womenoftech.files.wordpress.com/2016/07/talking-to-3.jpg?w=720" />			</div>
+		
+					<div class="entry-meta">
+				<span class="cat-links"><a href="https://womenoftech.wordpress.com/category/data/" rel="category tag">Data</a>, <a href="https://womenoftech.wordpress.com/category/feminism/" rel="category tag">Feminism</a>, <a href="https://womenoftech.wordpress.com/category/programming/" rel="category tag">Programming</a>, <a href="https://womenoftech.wordpress.com/category/talking-to/" rel="category tag">Talking to...</a>, <a href="https://womenoftech.wordpress.com/category/tech/" rel="category tag">Tech</a>, <a href="https://womenoftech.wordpress.com/category/tech-industry/" rel="category tag">Tech Industry</a></span>			</div><!-- .entry-meta -->
+		
+		<h1 class="entry-title">Talking to&#8230; Victoria Lloyd</h1>	</header><!-- .entry-header -->
+
+	<div class="entry-content">
+		<p style="text-align:left;padding:15px;background-color:#f7f7f7;font-family:'Helvetica Neue';"><img data-attachment-id="374" data-permalink="https://womenoftech.wordpress.com/2016/09/13/talking-to-abby-frear/abby-frear-01-01/" data-orig-file="https://womenoftech.files.wordpress.com/2016/09/abby-frear-01-01.png" data-orig-size="1667,1667" data-comments-opened="1" data-image-meta="{&quot;aperture&quot;:&quot;0&quot;,&quot;credit&quot;:&quot;&quot;,&quot;camera&quot;:&quot;&quot;,&quot;caption&quot;:&quot;&quot;,&quot;created_timestamp&quot;:&quot;0&quot;,&quot;copyright&quot;:&quot;&quot;,&quot;focal_length&quot;:&quot;0&quot;,&quot;iso&quot;:&quot;0&quot;,&quot;shutter_speed&quot;:&quot;0&quot;,&quot;title&quot;:&quot;&quot;,&quot;orientation&quot;:&quot;0&quot;}" data-image-title="Abby Frear TechSPARK Women of Tech" data-image-description="&lt;p&gt;Abby Frear TechSPARK Women of Tech&lt;/p&gt;
+//" data-medium-file="https://upload.wikimedia.org/wikipedia/commons/4/4d/Cat_November_2010-1a.jpg" data-large-file="https://upload.wikimedia.org/wikipedia/commons/4/4d/Cat_November_2010-1a.jpg" alt="Victoria Lloyd TechSPARK Women of Tech" width="97em" height="97em" /><b>Victoria Lloyd</b><br />
+Software Developer at Sky &#8211; <a href="https://www.sky.com?" target="_blank" rel="nofollow">LinkedIn</a></p>
+<p style="text-align:left;margin-left:40%;border-radius:10px;padding:15px;background-color:#dcf8c6;font-family:'Helvetica Neue';">Hi Victoria,<br />
+So, let’s start with an easy question. What do you do?</p>
+<p style="text-align:left;margin-right:40%;border-radius:10px;padding:15px;background-color:#f7f7f7;font-family:'Helvetica Neue';">I train women with no prior coding experience to be software developers at <a href="http://www.sky.com/" target="_blank" rel="nofollow">Sky</a>. I write in C# and PHP.</p>
+<p style="text-align:left;margin-left:40%;border-radius:10px;padding:15px;background-color:#dcf8c6;font-family:'Helvetica Neue';">And have you always worked in tech?</p>
+<p style="text-align:left;margin-right:40%;border-radius:10px;padding:15px;background-color:#f7f7f7;font-family:'Helvetica Neue';">Yes, since I was a little girl.</p>
+<p style="text-align:left;margin-left:40%;border-radius:10px;padding:15px;background-color:#dcf8c6;font-family:'Helvetica Neue';">Is it something you’ve always wanted to do?</p>
+<p style="text-align:left;margin-right:40%;border-radius:10px;padding:15px;background-color:#f7f7f7;font-family:'Helvetica Neue';">No, not at all. So, at school it was something that I’d thought about. You see people on TV doing really cool stuff in essentially the Matrix (or that’s what it looks like!) and I thought, “that looks awesome. Imagine if I could do that.”</p>
+<p style="text-align:left;margin-right:40%;border-radius:10px;padding:15px;background-color:#f7f7f7;font-family:'Helvetica Neue';">Then I went to school and I did ICT and they said, “Cool, here’s some stuff. It’s called Powerpoint…”<br />
+Yeah, so I dropped that pretty quickly.</p>
+<p style="text-align:left;margin-right:40%;border-radius:10px;padding:15px;background-color:#f7f7f7;font-family:'Helvetica Neue';">Just by chance I ended up doing work experience where I got to do some code and then came back the next summer and did some more experience and really enjoyed it.</p>
