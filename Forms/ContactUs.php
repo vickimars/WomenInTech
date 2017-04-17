3962fda321447a74fc0bc0668d77af0442031e87
Devina <devina.grewal@gmail.com>
2017-04-17 14:12:46 +0100

    contact us page

---
+++ Forms/ContactUs.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<form action="index.php">
+  First name:<br>
+  <input type="text" name="firstname" value="">
+  <br>
+  Last name:<br>
+  <input type="text" name="lastname" value="">
+  <br><br>
+  <input type="submit" value="Submit">
+</form> 
+
+
+
+</body>
+</html>
