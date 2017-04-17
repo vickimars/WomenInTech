147a4b9aa5853758a4f1ecd78bb52c833ada388a
Vicki Marshall <vickimars@hotmail.com>
2017-04-17 19:11:49 +0100

    updated index
    update

--- index.php
+++ index.php
@@ -3,6 +3,13 @@
 <!DOCTYPE html>
 <html>
     <?php
+// This is the one on github
+?>
+<!--
+
+<!DOCTYPE html>
+<html>
+    <?php
 // This is the home page of our application
 ?>
 
@@ -59,7 +66,7 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
 						 <ul class="nav navbar-nav">
 							<li class="hvr-bounce-to-bottom active"><a href="index.php">Home</a></li>
 							<li class="hvr-bounce-to-bottom"><a href="about.html">About</a></li>
-							<li class="hvr-bounce-to-bottom"><a href="testupload.php">Uploadpost</a></li>
+							<!--<li class="hvr-bounce-to-bottom"><a href="testupload.php">Uploadpost</a></li>-->
 							<li class="hvr-bounce-to-bottom"><a href="contact.html">Contact Us</a></li>
 						  </ul>
 						  <div class="sign-in">
@@ -85,10 +92,10 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
 	<div class="header-bottom">
 		<div class="header-bottom-top">
 			<ul>
-				<li><a href="#" class="g"> </a></li>
-				<li><a href="#" class="p"> </a></li>
-				<li><a href="#" class="facebook"> </a></li>
-				<li><a href="#" class="twitter"> </a></li>
+				<li><a href="https://www.facebook.com/womenintechnology.co.uk/" class="g"> </a></li>
+				<li><a href="https://twitter.com/womenintech?lang=en-gb" class="p"> </a></li>
+				<!--<li><a href="https://www.facebook.com/womenintechnology.co.uk/" class="facebook"> </a></li>
+				<li><a href="https://twitter.com/womenintech?lang=en-gb" class="twitter"> </a></li>-->
 			</ul>
 		</div>
 		<div class="clearfix"> </div>
@@ -123,23 +130,9 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
 							<div class="banner-inf">
 								<h3>Bridget Codes Diary</h3>
 								<p>The not so secret diary of a female coder</p>
-								<a href="about us.php">See More</a>
+								<a href="about.html">See More</a>
 							</div>
-						</li>
-						<li>
-							<div class="banner-inf">
-								<h3>Can a woman coder have it all</h3>
-								<p>"One thing that I never anticipated when starting this course was the extent to which it would take over all aspects of my life. 
-                                                                    We all heeded the warnings about setting aside evenings for coding and that this would be hard but ultimately fulfilling. My vocabulary has changed, mojitos, Netflix and chilling have been replaced with PHP, Trello and my all-time appropriately named favourite GitHub.  A three hour chunk of ‘free time’ takes on a new meaning. When I started the course I would allot time to study and find that two hours later, I was still staring at a line of code trying to get rock, paper, scissors to work (another sign that my priorities had changed). Friends would ask me what I was doing for the weekend and I would say ‘coding’, which wasn’t a euphemism for something alternative but an actual statement of fact.  I would be up until late at night coding and go to bed dreaming of PHP.</p>
-								<a href="single.html">See More</a>
-							</div>
-						</li>
-						<li>
-							<div class="banner-inf">
-								<h3>Hidden Figures Film Review</h3>
-								<p>Placeholder text</p>
-								<a href="single.html">See More</a>
-							</div>
+				
 						</li>
 					</ul>
 				</div>
@@ -211,7 +204,7 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
 				<div class="blog-left">
 					<div class="blog-left-grid">
 						<div class="blog-left-grid-left">
-							<h3><a href="single.html">Frustrations with Code</a></h3>
+							<h3><a href="single.html">Frustrations</a></h3>
 							<p>by <span>Christina G</span> | March 29,2017 | <span>Coder</span></p>
 						</div>
 						<div class="blog-left-grid-right">
@@ -253,13 +246,12 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
 								<div class="tab-1 resp-tab-content" aria-labelledby="tab_item-0">
 									<div class="facts">
 									  <div class="tab_list">
-										<a href="womenintech-master/web/images/itcrowd.gif" class="b-link-stripe b-animate-go   swipebox"  title="">
+										<a href="womenintech-master/web/images/funnybaby.jpg" class="b-link-stripe b-animate-go   swipebox"  title="">
 											<img src="images/7.jpg" alt=" " class="img-responsive" />
 										</a>
 									  </div>
 									  <div class="tab_list1">
-										<a href="#">excepturi sint occaecati</a>
-										<p>June 30,2015 <span>Nam libero tempore, cum soluta nobis.</span></p>
+							
 									  </div>
 									  <div class="clearfix"> </div>
 									</div>
@@ -270,8 +262,7 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
 										</a>
 									  </div>
 									  <div class="tab_list1">
-										<a href="#">excepturi sint occaecati</a>
-										<p>June 30,2015<span>Nam libero tempore, cum soluta nobis.</span></p>
+						
 									  </div>
 									  <div class="clearfix"> </div>
 									</div>
@@ -281,10 +272,7 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
 											<img src="images/9.jpg" alt=" " class="img-responsive" />
 										</a>
 									  </div>
-									  <div class="tab_list1">
-										<a href="#">excepturi sint occaecati</a>
-										<p>June 30,2015<span>Nam libero tempore, cum soluta nobis.</span></p>
-									  </div>
+									
 									  <div class="clearfix"> </div>
 									</div>
 									<div class="facts">
@@ -294,23 +282,14 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
 										</a>
 									  </div>
 									  <div class="tab_list1">
-										<a href="#">excepturi sint occaecati</a>
-										<p>June 30,2015<span>Nam libero tempore, cum soluta nobis.</span></p>
+						
 									  </div>
 									  <div class="clearfix"> </div>
 									</div>
 								</div>
 								<div class="tab-1 resp-tab-content" aria-labelledby="tab_item-1">
 									<div class="facts">
-									  <div class="tab_list">
-										<a href="images/8-.jpg" class="b-link-stripe b-animate-go   swipebox"  title="">
-											<img src="images/8.jpg" alt=" " class="img-responsive" />
-										</a>
-									  </div>
-									  <div class="tab_list1">
-										<a href="#">excepturi sint occaecati</a>
-										<p>June 30,2015<span>Nam libero tempore, cum soluta nobis.</span></p>
-									  </div>
+									  
 									  <div class="clearfix"> </div>
 									</div>
 									<div class="facts">
@@ -319,10 +298,7 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
 											<img src="images/9.jpg" alt=" " class="img-responsive" />
 										</a>
 									  </div>
-									  <div class="tab_list1">
-										<a href="#">excepturi sint occaecati</a>
-										<p>June 30,2015<span>Nam libero tempore, cum soluta nobis.</span></p>
-									  </div>
+							
 									  <div class="clearfix"> </div>
 									</div>
 									<div class="facts">
@@ -331,10 +307,7 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
 											<img src="images/10.jpg" alt=" " class="img-responsive" />
 										</a>
 									  </div>
-									  <div class="tab_list1">
-										<a href="#">excepturi sint occaecati</a>
-										<p>June 30,2015<span>Nam libero tempore, cum soluta nobis.</span></p>
-									  </div>
+									
 									  <div class="clearfix"> </div>
 									</div>
 									<div class="facts">
@@ -455,20 +428,18 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
 								<img src="WomenInTech-master/web/images/women2.jpg" class="img-responsive" alt=" " />
 							</a>
 						</div>
-						<div class="four-fig1">
+                                                <div class="four-fig1">
 							<a href="WomenInTech" class="b-link-stripe b-animate-go   swipebox"  title="">
-								<img src="WomenInTech-master/web/images/coding.png" class="img-responsive" alt=" " />
+								<img src="WomenInTechVM/images/baby.jpg" class="img-responsive" alt=" " />
 							</a>
 						</div>
 						<div class="clearfix"> </div>
 					</div>
 					<div class="pgs">
-						<h3>Pages</h3>
+						<h3><a href="#">Read older posts</a></h3>
 						<ul>
-							<li><a href="#">placeholder</a></li>
-							<li><a href="#">placeholder</a></li>
-							<li><a href="#">placeholder</a></li>
-							<li><a href="#">placeholder</a></li>
+							
+							
 							
 						</ul>
 					</div>
@@ -486,10 +457,10 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
 		<div class="container">
 			<div class="footer-grids">
 				<div class="col-md-3 footer-grid">
-					<h3>Footer</h3>
+	
 					</div>
 					<div class="footer-grd-left">
-						<p>placeholder</p>
+						<p>WomeninTech@2017</p>
 					</div>
 					<div class="clearfix"> </div>
 				</div>
@@ -497,25 +468,22 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
 					
 				</div>
 				<div class="col-md-3 footer-grid">
-					<h3>Pictures</h3>
+			
 					<div class="footer-grd">
-						<img src="WomenIntech-master/web/images/womenintech.jpg" class="img-responsive" alt=" " />
+					
 					</div>
 					<div class="footer-grd">
-						<img src="WomenIntech-master/web/images/women2.jpg" class="img-responsive" alt=" " />
+			
 					</div>
 					<div class="footer-grd">
-						<img src="WomenIntech-master/web/images/coding.png" class="img-responsive" alt=" " />
+		
 					<div class="clearfix"> </div>
 				</div>
 				<div class="clearfix"> </div>
 			</div>
 		</div>
 	</div>
-	<div class="footer-bottom">
-		<div class="container">
-		</div>
-	</div>
+	
 <!-- //footer -->
 <!-- for bootstrap working -->
 		<script src="js/bootstrap.js"> </script>
